67ff2a4a81b509cf1d6ac4c461663938963cebfd
trueSolju <jusol0701@gmail.com>
2024-02-22 16:14:00 +0900

    ãdev 브렌치 생성 및 코드 변경

--- src/main/java/com/example/GitTest/TestController.java
+++ src/main/java/com/example/GitTest/TestController.java
@@ -9,6 +9,7 @@ public class TestController {
     @GetMapping("/")
     public String main(){
         System.out.println(111111);
+        System.out.println("이 코드는 dev 브렌치에서 추가함");
         return "main";
     }
 }
